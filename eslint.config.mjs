1bd0223537718fd386409d33d2a70f30b32cb932
André Felipe Schulle <andrefelipeschulle@icloud.com>
2026-07-13 15:27:42 -0300

    fix: ajustes no sistema, suporte apenas para ESM

--- eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import nodeConfig from "@brainylab/eslint-config-node";
-import { typescriptConfig } from "@brainylab/eslint-config-typescript";
-
-export default [
-  ...typescriptConfig,
-  ...nodeConfig,
-  { rules: { "n/no-unsupported-features/node-builtins": "off" } },
-];
